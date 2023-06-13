a93be3ae5117367ba5af4f6008b43d58937da347
user <user@DESKTOP-B4PMNF6>
2023-06-13 16:40:35 +0900

    0613 16:40 exam2

---
+++ exam2/src/main/java/com/yedam/java/book/service/BookVO.java
@@ -0,0 +1,25 @@
+package com.yedam.java.book.service;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BookVO {
+	private int bookNo;
+	private String bookName;
+	private String bookCoverimg;
+	// 기본 : yyyy/MM/dd
+	private Date bookDate;
+	private int bookPrice;
+	private String bookPublisher;
+	private String bookInfo;
+	
+//	public void setBookPrice(String bookPrice) {
+//		if(bookPrice == null || !bookPrice.equals("")) {
+//			this.bookPrice = 0;
+//		} else {
+//			this.bookPrice = Integer.parseInt(bookPrice);
+//		}
+//	}
+}
