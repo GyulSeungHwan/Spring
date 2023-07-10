2ce265ce5e4b17a736a5078e6dcb4e451328dc9f
user <user@DESKTOP-B4PMNF6>
2023-07-10 09:58:21 +0900

    asd

---
+++ boot/src/main/java/com/yedam/app/emp/controller/EmpController.java
@@ -0,0 +1,58 @@
+package com.yedam.app.emp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.yedam.app.emp.service.EmpService;
+import com.yedam.app.emp.service.EmpVO;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+	EmpService empService;
+	
+	// 전체조회 페이지
+	@GetMapping("empList")
+	public String empList(Model model) {
+		model.addAttribute("empList", empService.getEmpList());
+		return "emp/empList";
+		// 폴더 안에 들어있다고 /empList로 하면 안된다(리눅스에서 에러남)
+		// 대소문자 구분해주기(리눅스에서 에러남)
+	}
+	
+	// 단건조회 페이지
+	@GetMapping({"empInfo/{employeeId}", "empInfo"})
+	public String empInfo(EmpVO empVO, Model model, @PathVariable(required = false) int employeeId) {
+		if(employeeId != 0)
+			empVO.setEmployeeId(employeeId);
+		model.addAttribute("empInfo", empService.getEmpInfo(empVO));
+		return "emp/empInfo";
+	}
+	
+	// 등록 페이지
+	@GetMapping("empInsert")
+	public String empInsertForm(Model model) {
+		model.addAttribute("empVO", new EmpVO());
+		// 여기서 넘겨주는 "empVO"는 다른 컨트롤의 매개변수와도 이름이 같아야한다
+		return "emp/empInsert";
+	}
+	
+	// 등록 처리
+	@PostMapping("empInsert")
+	public String empInsert(EmpVO empVO) {
+		empService.insertEmpInfo(empVO);
+		return "redirect:empList";
+	}
+	
+	// 수정 페이지
+	@GetMapping("empUpdate")
+	public String empUpdateForm(Model model, EmpVO vo) {
+		model.addAttribute("empVO", empService.getEmpInfo(vo));
+		return "emp/empUpdate";
+	}
+}
